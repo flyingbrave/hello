b44be2beb3f13067cc7a1c7f311391bae3cc944a
runingWoniu <1150498563@qq.com>
2021-08-30 19:53:24 +0800

    hello代码第一次提交

---
+++ plugin/src/main/groovy/com/yxy/hello/plugin/AppJointPlugin.groovy
@@ -0,0 +1,13 @@
+package com.yxy.hello.plugin
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AppJointPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.android.registerTransform(new AppJointTransform(project))
+    }
+}
