c192b557a1aafa137402df40bc89516d1175024b
yunxiangyong <yunxiangyong@zuoyebang.com>
2021-08-31 17:56:31 +0800

    添加单独运行的model

---
+++ independent/runmodel2/src/main/java/com/yxy/runmodel2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yxy.runmodel2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.yxy.common.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
